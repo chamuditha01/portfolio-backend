7c0321b6ee0e6e9af1d5017745d7e8f3b5cbdbac
chamuditha01 <heshanchamuditha05@gmail.com>
2024-06-21 14:27:56 +0530

    ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 
-const app = express();
+const  app = express();
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
